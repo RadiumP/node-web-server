7e49fed6c055ba34a46d523fa031fbda46c12519
RadiumP <chenyan5@asu.edu>
2017-04-03 19:25:36 -0700

    Setuo start script and heroku

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+//heroku
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -72,5 +75,7 @@ app.get('/bad', (req, res)=>{
     });
 });
 
-app.listen(3000);
+app.listen(port, ()=>{
+    console.log(`Server up on port ${port}`);
+});
 
